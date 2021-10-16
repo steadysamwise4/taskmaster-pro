bdaa6a614bb8178918a6236c1ec90df43194fd5d
Sam Davenport <samueldavenport@att.net>
2021-10-16 08:22:19 -0500

    added draggability of tasks, added red area to drop/delete, made mobile friendly - application is buggy when moving tasks to in review and done columns-tasks disappear on refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    //console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -164,6 +164,71 @@ $("#task-form-modal .btn-primary").click(function() {
     });
 
     saveTasks();
+  }
+});
+
+// implement sortable and dragable tasks using jQuery UI
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    //console.log("activate", this);
+  },
+  deactivate: function(event) {
+    //console.log("deactivate", this);
+  },
+  over: function(event) {
+    //console.log("over", event.target);
+  },
+  out: function(event) {
+    //console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+      
+    });
+    console.log(tempArr);
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+    
+  }
+});
+
+$("#trash").droppable( {
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    
+  },
+  out: function(event, ui) {
+    
   }
 });
 
